59240463f5d15b1944530c22c421691519b0a5aa
Programator <26222619+MatorProgra@users.noreply.github.com>
2022-02-08 19:40:02 +0100

    New version in .NET 6

---
+++ RandomNumberGame/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random random = new Random();
+int number = random.Next(101);
+int iterations = 0;
+
+Console.Write("Wpisz szukaną liczbę (od 0 do 100): ");
+
+while (true)
+{
+	if (int.TryParse(Console.ReadLine(), out int input) && input >= 0 && input <= 100)
+	{
+		iterations++;
+
+		if (input > number)
+			Console.Write("Podana liczba jest większa od szukanej. Spróbuj jeszcze raz: ");
+		else if (input < number)
+			Console.Write("Podana liczba jest mniejsza od szukanej. Spróbuj jeszcze raz: ");
+		else
+		{
+			Console.Write($"Brawo! Znalazłeś poprawną liczbę - {number} w {iterations} krokach.");
+			break;
+		}
+	}
+	else
+		Console.Write("Error! błędne dane! Spróbuj jeszcze raz (liczba od 0 do 100): ");
+}
+Console.ReadLine();
